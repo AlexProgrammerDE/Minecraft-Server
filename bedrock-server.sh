9e147f9f6ed06d002046b1348b94a86cb28de32e
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-11-23 16:46:08 +0100

    Update bedrock-server.sh

--- bedrock-server.sh
+++ bedrock-server.sh
@@ -1,5 +1,31 @@
 #!/bin/bash
 
-if [[ $1 == 1.13.3.0 ]]; then
+HEIGHT=20
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="Minecraft-Server"
 
-fi
+TITLE="Agreement"
+MENU="When you chose yes you agree to the Minecraft End User License Agreement: https://minecraft.net/terms and Privacy Policy: https://go.microsoft.com/fwlink/?LinkId=521839"
+
+OPTIONS=(1 "Yes"
+         2 "No")
+         
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+                
+case $CHOICE in
+        1)
+            if [[ $1 == latest ]]; then
+               wget -O bedrock-server.zip https://minecraft.azureedge.net/bin-linux/bedrock-server-1.13.3.0.zip
+               unzip bedrock-server.zip
+               cd bedrock-server
+               ./bedrock_server 
+            fi
+            ;;
+esac
