4cb14a4c45eda77c541ebe40b44f1d767c6b2ab7
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2019-11-30 15:32:35 +0100

    Add installer

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y dialog
+
+cd /opt/
+sudo rm -r Minecraft-Server
+git clone https://github.com/AlexProgrammerDE/Minecraft-Server.git
+
+source /opt/Minecraft-Server/start.sh
+echo "source /opt/Minecraft-Server/start.sh" > ~/.bashrc
